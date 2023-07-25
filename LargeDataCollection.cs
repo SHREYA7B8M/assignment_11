e945df4169d74310bf001cfb4092e0cf7d5cd25a
s66829020@gmail.com <s66829020@gmail.com>
2023-07-26 00:03:14 +0530

    first commit

---
+++ LargeDataCollection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+public class LargeDataCollection : IDisposable
+{
+    private List<string> data;
+    static int totalFiles = 0;
+
+    public LargeDataCollection(IEnumerable<string> initialData)
+    {
+        data = new List<string>(initialData);
+
+        totalFiles++;
+
+        Console.WriteLine("File Created");
+        Console.WriteLine("\n# of files are: " + totalFiles);
+
+    }
+
+    public void Add(string element)
+    {
+        data.Add(element);
+    }
+
+    public bool Remove(string element)
+    {
+        return data.Remove(element);
+    }
+
+    public string this[int index]
+    {
+        get { return data[index]; }
+        set { data[index] = value; }
+    }
+
+    public int Count => data.Count;
+
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            if(data!= null)
+            {
+                data.Clear();
+                data = null;
+            }
+           totalFiles = 0;
+
+        }
+        Console.WriteLine($"\nFile has been disposed...");
+
+       
+
+    }
+
+    ~LargeDataCollection()
+    {
+        Dispose(false);
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    public static LargeDataCollection agent { get; private set; }
+
+    static void Main()
+    {
+        agent = new LargeDataCollection(new string[] { "\nEthan Hunt", "Luther Stickell", "Benji Dunn", "Ilsa Faust", "Alanna Mitsopolis" });
+        {
+
+            agent.Add("Eugene Kittridge");
+            agent.Add("Jasper Briggs");
+
+            Console.WriteLine("\nAccessing Agents");
+            Console.WriteLine(agent[0]);
+            Console.WriteLine(agent[1]);
+            Console.WriteLine(agent[2]);
+            Console.WriteLine(agent[3]);
+            //Console.WriteLine(agent[4]);
+            //Console.WriteLine(agent[5]);
+            //Console.WriteLine(agent[6]);
+
+            agent.Remove("Ilsa Faust");
+
+            Console.WriteLine("\nCollection after removing 'Ilsa Faust':\n");
+
+            Console.WriteLine("Agents Count: " + agent.Count);
+
+            for (int i = 0; i < agent.Count; i++)
+            {
+                Console.WriteLine(agent[i]);
+            }
+
+            agent.Dispose();
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
